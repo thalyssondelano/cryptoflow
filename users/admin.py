038a2ea4f3bac07cc1fef165f0d90fdf30a38e45
thalyssondelano <contato.thalyssonde@gmail.com>
2026-08-31 20:52:28 -0300

    feat: aplica regras de validacao de registro de usuarios no painel admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser, Wallet
+from django.contrib.auth.admin import UserAdmino 
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Wallet)
