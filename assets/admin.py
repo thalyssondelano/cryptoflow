42869ef23aab04b3a8697ca8d5c6988c4c3378c3
thalyssondelano <contato.thalyssondel@gmail.com>
2026-08-31 23:23:56 -0300

    feat: adiciona tabela de criptomoedas no painel admin

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CryptoCurrency
 
-# Register your models here.
+@admin.register(CryptoCurrency)
+class CryptoCurrencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'symbol', 'current_price', 'is_active', 'updated_at')
+    search_fields = ('name', 'symbol')
+    list_filter = ('is_active',)
