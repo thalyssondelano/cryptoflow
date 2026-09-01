7e3e2b82b59d5d21421631ece90d1e525fc63a14
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 13:52:50 -0300

    feat: adiciona a rota para a view TradeHistory no arquivo de URLs

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserProfileView, BuyCryptoView, SellCryptoView
+from .views import UserRegisterView, UserProfileView, BuyCryptoView, SellCryptoView, TradeHistoryView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('me/', UserProfileView.as_view(), name='user-profile'),
     path('buy/', BuyCryptoView.as_view(), name='buy_crypto'),
     path('sell/', SellCryptoView.as_view(), name='sell_crypto'),
+    path('history/', TradeHistoryView.as_view(), name='trade-history')
 ]
