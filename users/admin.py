13fd7ceebf894d4795b97178e057b44eab6fd859
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 15:34:31 -0300

    fix: corrige bug de usuario poder ser criado sem email no painel admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
 from .models import CustomUser, Wallet, WalletAsset, TradeHistory
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username')
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+    )
+
+    list_display = ('email', 'username', 'is_staff')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('username',)}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(Wallet)
 class WalletAdmin(admin.ModelAdmin):
