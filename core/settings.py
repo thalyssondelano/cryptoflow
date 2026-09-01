182462dde54b15263d4eaf97e22b271214e82924
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 14:43:33 -0300

    feat: adiciona conexao com o postgres no settings.py

--- core/settings.py
+++ core/settings.py
@@ -64,10 +64,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cryptoflow_db',
+        'USER': 'admin',
+        'PASSWORD': 'adminpassword',
+        'HOST': 'db', 
+        'PORT': '5432',
     }
 }
 
