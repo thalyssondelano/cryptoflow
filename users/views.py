b275b0be022f58d6deeeaa62cf08ad190bf289d1
thalyssondelano <contato.thalyssonde@gmail.com>
2026-08-31 21:02:07 -0300

    feat: adiciona a generic view de registro de usuarios

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
