5a976b44bb696ba5d42c881337367c112cf0d3d8
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 02:05:58 -0300

    feat: adiciona WalletAssets no painel admin e aprimora amostragem de dados

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import CustomUser, Wallet
+from .models import CustomUser, Wallet, WalletAsset
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Wallet)
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
+    search_fields = ('user__username', 'user__email')
+
+@admin.register(WalletAsset)
+class WalletAssetAdmin(admin.ModelAdmin):
+    list_display = ('get_username', 'crypto', 'quantity')
+    search_fields = ('wallet__user__username', 'crypto__symbol') 
+    
+    def get_username(self, obj):
+        return obj.wallet.user.username
+    get_username.short_description = 'Usuário'
