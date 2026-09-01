526c978b21fbf45f59a34fb926bf4ebd6240eb03
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 00:17:29 -0300

    feat: adiciona rota de listagem de dados de criptomoedas

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import CryptoCurrency
+from .serializers import CryptoCurrencySerializer
 
-# Create your views here.
+class CryptoCurrencyListView(generics.ListAPIView):
+    queryset = CryptoCurrency.objects.filter(is_active=True)
+    serializer_class = CryptoCurrencySerializer
+    permission_classes = [IsAuthenticated]
