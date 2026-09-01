a31aa134850a46101ad1f632fa5b57d04adcea44
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 02:36:49 -0300

    feat: adiciona url sell em users

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserProfileView, BuyCryptoView
+from .views import UserRegisterView, UserProfileView, BuyCryptoView, SellCryptoView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
 
     path('me/', UserProfileView.as_view(), name='user-profile'),
     path('buy/', BuyCryptoView.as_view(), name='buy_crypto'),
+    path('sell/', SellCryptoView.as_view(), name='sell_crypto'),
 ]
