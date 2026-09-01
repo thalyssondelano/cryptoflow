36624c469e1f9ba1c77bbf71f53cd6ff0bf04fdf
thalyssondelano <contato.thalyssondel@gmail.com>
2026-09-01 03:11:14 -0300

    feat: registra o modelo TradeHistory no admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Wallet, WalletAsset
+from .models import CustomUser, Wallet, WalletAsset, TradeHistory
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(CustomUser, UserAdmin)
@@ -14,6 +14,17 @@ class WalletAssetAdmin(admin.ModelAdmin):
     list_display = ('get_username', 'crypto', 'quantity')
     search_fields = ('wallet__user__username', 'crypto__symbol') 
     
+    def get_username(self, obj):
+        return obj.wallet.user.username
+    get_username.short_description = 'Usuário'
+
+@admin.register(TradeHistory)
+class TradeHistoryAdmin(admin.ModelAdmin):
+    list_display = ('get_username', 'trade_type', 'crypto', 'quantity', 'usd_amount', 'timestamp')
+    list_filter = ('trade_type', 'timestamp')
+    search_fields = ('wallet__user__username', 'crypto__symbol')
+    readonly_fields = ('timestamp', 'wallet', 'crypto', 'trade_type', 'quantity', 'price_at_transaction', 'usd_amount') 
+
     def get_username(self, obj):
         return obj.wallet.user.username
     get_username.short_description = 'Usuário'
